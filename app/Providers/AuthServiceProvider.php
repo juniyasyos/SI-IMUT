c3481ad642ff62a00caa7d3b4eda38b165c0d670
Juniyasyos <juniyasyos@gmail.com>
2025-05-18 10:53:17 +0700

    fix policy media manager

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,12 +4,16 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use App\Policies\ActivityPolicy;
+use App\Policies\FolderPolicy;
+use App\Policies\MediaPolicy;
 use Spatie\Activitylog\Models\Activity;
 
 class AuthServiceProvider extends ServiceProvider
 {
     protected $policies = [
         Activity::class => ActivityPolicy::class,
+        \TomatoPHP\FilamentMediaManager\Models\Folder::class => FolderPolicy::class,
+        \TomatoPHP\FilamentMediaManager\Models\Media::class => MediaPolicy::class,
     ];
 
     public function boot(): void
