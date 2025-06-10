8d8430f8a311a12679b18e72b5dad73822c866ef
juniyasyos <juniyasyos@gmail.com>
2025-06-10 16:47:18 +0700

    fix permission media manager (delete media) and add view media manager

--- app/Filament/Resources/MediaCustomResource/Pages/EditMediaCustom.php
+++ app/Filament/Resources/MediaCustomResource/Pages/EditMediaCustom.php
@@ -2,18 +2,19 @@
 
 namespace App\Filament\Resources\MediaCustomResource\Pages;
 
-use Juniyasyos\FilamentMediaManager\Resources\MediaResource;
+use App\Filament\Resources\MediaCustomResource;
 use Filament\Actions;
 use Juniyasyos\FilamentMediaManager\Resources\MediaResource\Pages\EditMedia;
 
 class EditMediaCustom extends EditMedia
 {
-    protected static string $resource = MediaResource::class;
+    protected static string $resource = MediaCustomResource::class;
 
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            Actions\DeleteAction::make()
+                ->visible(fn () => \Illuminate\Support\Facades\Gate::any(['delete_media::custom'])),
         ];
     }
 }
